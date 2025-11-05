46e131160da1d3ff8d59a1a139962a6e1e004cd9
JaeYeon Kim <ghgh415263@naver.com>
2025-11-05 21:14:33 +0900

    GH-56 createdAt, modifiedAt을 db에서 처리하도록 변경
    1. columnDefinition을 이용하여 해당 컬럼에 DEFAULT 혹은 ON UPDATE를 설정
    2. 어플리케이션에서 해당값을 설정하지 못하게 updatable = false, insertable = false

--- src/main/java/com/example/study/common/persistance/BaseUpdateEntity.java
+++ src/main/java/com/example/study/common/persistance/BaseUpdateEntity.java
@@ -6,7 +6,6 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.hibernate.envers.Audited;
 import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -27,9 +26,8 @@ public abstract class BaseUpdateEntity extends BaseEntity {
 
     /**
      * 마지막 수정 시간
-     * 엔티티가 수정될 때 자동으로 현재 시간으로 갱신됨
+     * 엔티티가 수정될 때 DB에서 자동으로 현재 시간으로 갱신됨
      */
-    @LastModifiedDate
-    @Column(nullable = false)
+    @Column(updatable = false, insertable = false, nullable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
     private LocalDateTime modifiedAt;
 }
