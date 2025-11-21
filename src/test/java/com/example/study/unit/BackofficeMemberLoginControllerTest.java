2cfb6dc4adb0a1e36f0b61677eb3b1f9ba06970a
chungNyong <cndsud417@gmail.com>
2025-11-22 02:49:01 +0900

    backOffice 로그인 테스트 추가

--- src/test/java/com/example/study/unit/BackofficeMemberLoginControllerTest.java
+++ src/test/java/com/example/study/unit/BackofficeMemberLoginControllerTest.java
@@ -2,38 +2,28 @@ package com.example.study.unit;
 
 import com.example.study.common.authentication.JwtConfig;
 import com.example.study.common.authentication.backoffice.BackofficeTokenManager;
+import com.example.study.member.command.application.BackofficeMemberLoginDto;
+import com.example.study.member.command.application.BackofficeMemberLoginSaveForm;
 import com.example.study.member.command.application.BackofficeMemberLoginService;
-import com.example.study.member.test.TestBackofficeMemberLoginController;
-import jakarta.servlet.http.Cookie;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
-import jakarta.validation.ValidatorFactory;
-import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
+import com.example.study.member.command.application.InvalidCredentialsException;
+import com.example.study.member.ui.backoffice.BackofficeMemberLoginController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.validation.DefaultMessageCodesResolver;
-import org.springframework.validation.MessageCodesResolver;
 
-import java.util.Set;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(TestBackofficeMemberLoginController.class)
+@WebMvcTest(BackofficeMemberLoginController.class)
 @Import(JwtConfig.class)
 public class BackofficeMemberLoginControllerTest {
 
@@ -47,88 +37,77 @@ public class BackofficeMemberLoginControllerTest {
     private BackofficeTokenManager backofficeTokenManager;
 
     @Test
-    @DisplayName("로그인 성공 시 토큰생성되고 응답은 200")
-    void loginSuccessful_tokenCreated_ReturnsSuccess() throws Exception {
-        // given
-        long memberId = 1L;
-        String token = "mocked-jwt-token";
-
-        given(backofficeMemberLoginService.login(any())).willReturn(memberId);
-        given(backofficeTokenManager.generateToken(memberId)).willReturn(token);
-
-        // when
-        MvcResult result = mockMvc.perform(post("/backoffice/api/login")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("""
-                            {
-                                "loginId": "master",
-                                "password": "masterPassword123!"
-                            }
-                        """))
+    @DisplayName("로그인 폼을 받는다.")
+    void getloginView() throws Exception {
+        mockMvc.perform(get("/backoffice/login"))
                 .andExpect(status().isOk())
-                .andExpect(cookie().value("BACKOFFICE_AUTH_TOKEN", token))
-                .andReturn();
-
-        Cookie tokenCookie = result.getResponse().getCookie("BACKOFFICE_AUTH_TOKEN");
-        assertThat(tokenCookie).isNotNull();
-        assertThat(tokenCookie.getValue()).isEqualTo(token);
-    }
-
-    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
-
-    @Test
-    @DisplayName("DefaultMessageCodesResolver object 규칙을 테스트 한다.")
-    void defaultMessageCodesResolverRules_Object() {
-        // given
-        String[] messageCodes = codesResolver.resolveMessageCodes("required", "testObject");
-        // then
-        assertThat(messageCodes).containsExactly(
-                "required.testObject"
-                ,"required");
+                .andExpect(content().string(containsString("<form")))
+                .andExpect(content().string(containsString("name=\"loginId\"")))
+                .andExpect(content().string(containsString("name=\"password\"")))
+                .andDo(print());
     }
 
     @Test
-    @DisplayName("DefaultMessageCodesResolver field 규칙을 테스트 한다.")
-    void defaultMessageCodesResolverRules_Field() {
-        // given
-        String[] messageCodes = codesResolver.resolveMessageCodes("required", "testObject", "testObjectName", String.class);
-        // then
-        assertThat(messageCodes).containsExactly(
-                "required.testObject.testObjectName"
-                ,"required.testObjectName"
-                ,"required.java.lang.String"
-                ,"required"
-        );
-    }
-
-    // Test for beanValidation_NotBlank
-    @Getter
-    @Setter
-    static class TestLoginForm {
-        @NotBlank(message = "loginId는 필수입니다.")
-        private String loginId;
-
-        @NotBlank(message = "password는 필수입니다.")
-        private String password;
+    @DisplayName("로그인 성공시 backOffice jwt가 발급되고 home 화면으로 redirect 한다.")
+    void loginSuccessful_home_redirect() throws Exception {
+        mockMvc.perform(post("/backoffice/login")
+                .param("loginId", "master")
+                .param("password", "masterPassword123!"))
+                /* redirect 검증 */
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/backoffice/home"))
+                /*  토큰 검증 */
+                .andExpect(cookie().exists("BACKOFFICE_AUTH_TOKEN"))
+                .andExpect(cookie().httpOnly("BACKOFFICE_AUTH_TOKEN", true))
+                .andExpect(cookie().secure("BACKOFFICE_AUTH_TOKEN", true))
+                .andExpect(header().string(HttpHeaders.SET_COOKIE, containsString("SameSite=Strict")))
+                .andExpect(cookie().path("BACKOFFICE_AUTH_TOKEN", "/"))
+                .andExpect(cookie().maxAge("BACKOFFICE_AUTH_TOKEN", 3600));
     }
 
     @Test
-    @DisplayName("Bean Validation NotBlank 규칙을 테스트 한다.")
-    void beanValidation_NotBlank() {
+    @DisplayName("잘못된 아이디 패스워드로 로그인시 실패시 에러 메세지와 함께 home 화면으로 redirect 한다.")
+    void loginFailRedirectHome() throws Exception {
         // given
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        Validator validator = factory.getValidator();
-
-        TestLoginForm login = new TestLoginForm();
-        login.setLoginId("  ");
-        login.setPassword("");
+        BackofficeMemberLoginSaveForm saveForm = new BackofficeMemberLoginSaveForm("testId", "testPassword");
+        BackofficeMemberLoginDto loginDto = new BackofficeMemberLoginDto(saveForm.getLoginId(),saveForm.getPassword());
 
         // when
-        Set<ConstraintViolation<TestLoginForm>> violations = validator.validate(login);
+        doThrow(new InvalidCredentialsException())
+                .when(backofficeMemberLoginService)
+                .login(loginDto);
 
         // then
-        assertThat(violations.stream().map(ConstraintViolation::getMessage).toList()).containsExactlyInAnyOrder(
-                "loginId는 필수입니다.", "password는 필수입니다."
-        );
+        mockMvc.perform(post("/backoffice/login")
+                .param("loginId", "testId")
+                .param("password", "testPassword"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/backoffice/login"))
+                .andExpect(flash().attributeExists("login"))
+                .andExpect(flash().attribute("login",
+                        hasProperty("loginId", equalTo(saveForm.getLoginId()))
+                ))
+                .andExpect(flash().attribute("login",
+                        hasProperty("password", equalTo(saveForm.getPassword()))
+                ))
+                .andExpect(flash().attribute("loginError", "아이디 또는 비밀번호가 올바르지 않습니다."))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("아이디 패스워드 공백일 경우 BindingResult를 반환한다.")
+    void loginValidation() throws Exception {
+        // given
+        BackofficeMemberLoginSaveForm saveForm = new BackofficeMemberLoginSaveForm("", "");
+
+        // then
+        mockMvc.perform(post("/backoffice/login")
+                        .param("loginId", saveForm.getLoginId())
+                        .param("password", saveForm.getPassword()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("backoffice/login"))
+                .andExpect(model().attributeExists("login"))
+                .andExpect(model().attributeHasFieldErrors("login", "loginId"))
+                .andExpect(model().attributeHasFieldErrors("login", "password"));
     }
 }
---
+++ src/test/java/com/example/study/unit/TestBackofficeMemberLoginControllerTest.java
@@ -0,0 +1,65 @@
+package com.example.study.unit;
+
+import com.example.study.common.authentication.JwtConfig;
+import com.example.study.common.authentication.backoffice.BackofficeTokenManager;
+import com.example.study.member.command.application.BackofficeMemberLoginService;
+import com.example.study.member.test.TestBackofficeMemberLoginController;
+import jakarta.servlet.http.Cookie;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TestBackofficeMemberLoginController.class)
+@Import(JwtConfig.class)
+public class TestBackofficeMemberLoginControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private BackofficeMemberLoginService backofficeMemberLoginService;
+
+    @MockitoBean
+    private BackofficeTokenManager backofficeTokenManager;
+
+    @Test
+    @DisplayName("로그인 성공 시 토큰생성되고 응답은 200")
+    void loginSuccessful_tokenCreated_ReturnsSuccess() throws Exception {
+        // given
+        long memberId = 1L;
+        String token = "mocked-jwt-token";
+
+        given(backofficeMemberLoginService.login(any())).willReturn(memberId);
+        given(backofficeTokenManager.generateToken(memberId)).willReturn(token);
+
+        // when
+        MvcResult result = mockMvc.perform(post("/backoffice/api/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                            {
+                                "loginId": "master",
+                                "password": "masterPassword123!"
+                            }
+                        """))
+                .andExpect(status().isOk())
+                .andExpect(cookie().value("BACKOFFICE_AUTH_TOKEN", token))
+                .andReturn();
+
+        Cookie tokenCookie = result.getResponse().getCookie("BACKOFFICE_AUTH_TOKEN");
+        assertThat(tokenCookie).isNotNull();
+        assertThat(tokenCookie.getValue()).isEqualTo(token);
+    }
+}
